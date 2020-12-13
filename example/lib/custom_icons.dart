1d324f718502a2eac39128b3a2e49dd9af1ed1ad
Viktor <v.golstein@gmail.com>
2020-12-13 21:52:04 +0100

    fixed formatting

--- example/lib/custom_icons.dart
+++ example/lib/custom_icons.dart
@@ -5,13 +5,9 @@ class CustomIcons {
 
   static const _kFontFamily = 'IconFont';
 
-  static const IconData calendar =
-      const IconData(0xe800, fontFamily: _kFontFamily);
-  static const IconData podcasts =
-      const IconData(0xe801, fontFamily: _kFontFamily);
-  static const IconData home = const IconData(0xe802, fontFamily: _kFontFamily);
-  static const IconData search =
-      const IconData(0xe803, fontFamily: _kFontFamily);
-  static const IconData tickets =
-      const IconData(0xe804, fontFamily: _kFontFamily);
+  static const IconData calendar = IconData(0xe800, fontFamily: _kFontFamily);
+  static const IconData podcasts = IconData(0xe801, fontFamily: _kFontFamily);
+  static const IconData home = IconData(0xe802, fontFamily: _kFontFamily);
+  static const IconData search = IconData(0xe803, fontFamily: _kFontFamily);
+  static const IconData tickets = IconData(0xe804, fontFamily: _kFontFamily);
 }
